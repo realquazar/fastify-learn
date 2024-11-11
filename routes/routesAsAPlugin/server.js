5d0d382fddf43bd7756a54a98f7bf1442ae89548
Quazar <112936536+realquazar@users.noreply.github.com>
2024-11-11 20:51:16 +0530

    Create server.js

---
+++ routes/routesAsAPlugin/server.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({logger: true})
+fastify.register(require('./routes/items'))
+
+const PORT = 5000
+
+
+
+const start = async () => {
+    try {
+        await fastify.listen({port: PORT})
+        
+    } catch (error) {
+        fastify.log.error(error)
+        process.exit(1)        
+    }
+}
+start();
