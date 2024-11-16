6b91f4eba2d79a86579284c5740cc29de455f916
Quazar <112936536+realquazar@users.noreply.github.com>
2024-11-16 13:14:43 +0530

    Create items.js

---
+++ addDeleteUpdate/controllers/items.js
@@ -0,0 +1,46 @@
+const { v4: uuidv4} = require('uuid')
+let items = require('../Items')
+
+const getItems = (request, reply) => {
+    reply.send(items)
+}
+
+const getItem = (request, reply) => {
+    const {id} = request.params
+    const item = items.find(item => item.id === id)
+    reply.send(item)
+}
+
+const addItem = (request, reply) => {
+    const {name} = request.body
+    const item = {
+        id: uuidv4(),
+        name
+    }
+    items = [...items, item]
+    reply.code(201).send(item)
+}
+
+const deleteItem = (request, reply) => {
+    const {id} = request.params
+    items = items.filter(item => item.id !== id)
+    reply.send({message: `Item ${id} deleted`})
+
+}
+
+const updateItem = (request, reply) => {
+    const {id } = request.params
+    const {name} = request.body
+    items = items.map(item => (item.id === id) ? {id, name} : item)
+    item = items.find((item) => item.id === id)
+    reply.send(item)
+
+}
+
+module.exports = {
+    getItems,
+    getItem,
+    addItem,
+    deleteItem,
+    updateItem
+}
